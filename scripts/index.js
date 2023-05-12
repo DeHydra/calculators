0731cc3e619b722ce1f94b7616b1d6c7ed22d85c
DeHydra <littleacegh@gmail.com>
2023-05-12 23:06:12 +0300

    0.0.1

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+const LINKS = ['/pages/combinedMean.html'];
+
+let CONTENT = document.querySelector('.content');
+
+console.log(CONTENT);
+
+let calculators;
+
+// CONTENT.children.forEach(item => {
+//     calculators.push(item);
+
+//     console.log(item);
+// });
+
+// for (let i = 0; i < calculators.length; i++) {
+//     console.log(i);
+//     calculators[i].addEventListener('click', () => {
+//             console.log('нажали');
+//             window.location.pathname = LINKS[i];
+//         });    
+// }
